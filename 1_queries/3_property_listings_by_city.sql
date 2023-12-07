378d18d0fba6fbb098786f06f9bb9c3ee14cd45a
dhaaranisuriya2605@gmail.com <dhaaranisuriya2605@gmail.com>
2023-12-06 22:52:42 -0800

    Show specific details(id, title, cost_per_night) about properties located in Vancouver including their average rating using SELECT query.

---
+++ 1_queries/3_property_listings_by_city.sql
@@ -0,0 +1,8 @@
+Select properties.id, title, cost_per_night,AVG(property_reviews.rating) AS average_rating
+from properties
+LEFT JOIN property_reviews ON property_reviews.property_id=properties.id
+WHERE city LIKE '%ancouv%'
+GROUP BY properties.id
+HAVING AVG(property_reviews.rating)>=4
+ORDER BY cost_per_night
+LIMIT 10;
