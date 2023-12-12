7aa41ba0e519b454695539fcb126323736c82aed
dhaaranisuriya2605@gmail.com <dhaaranisuriya2605@gmail.com>
2023-12-12 13:26:12 -0800

    In addUser func,insert the new user into the database and return a promise that resolves with the new user object.

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -62,10 +62,20 @@ const getUserWithId = function (id) {
  * @return {Promise<{}>} A promise to the user.
  */
 const addUser = function (user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
+  // const userId = Object.keys(users).length + 1;
+  // user.id = userId;
+  // users[userId] = user;
+  // return Promise.resolve(user);
+  console.log("inside addUser func ");
+  return pool
+    .query(`INSERT INTO users(name,email,password) VALUES($1,$2,$3) RETURNING *;`, [user.name, user.email, user.password]) // tristanjacobs@gmail.com
+    .then((result) => {
+      console.log("user:", result.rows[0]);
+      return result.rows[0] || null;
+    })
+    .catch((err) => {
+      console.log("Error::", err.message);
+    });
 };
 
 /// Reservations
