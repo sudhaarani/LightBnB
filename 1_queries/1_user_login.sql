d0950b89ff0a53c3e16806ec0e47e952119c9476
dhaaranisuriya2605@gmail.com <dhaaranisuriya2605@gmail.com>
2023-12-13 17:11:01 -0800

    add comments

--- 1_queries/1_user_login.sql
+++ 1_queries/1_user_login.sql
@@ -1,2 +1,6 @@
+-- Get details about a single user.
+-- Select their id, name, email, and password.
+-- Select a single user using their email address. Use tristanjacobs@gmail.com for now.
+
 SELECT id,name,email,password FROM users
 WHERE email='tristanjacobs@gmail.com';
