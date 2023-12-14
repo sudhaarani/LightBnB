5d1744f6129ecc82d7eb9ac1c654eb6e3c2cdfd3
dhaaranisuriya2605@gmail.com <dhaaranisuriya2605@gmail.com>
2023-12-13 17:07:29 -0800

    change group by clause to add properties.id instead of title and cost_per_night

--- 1_queries/5_all_my_reservations.sql
+++ 1_queries/5_all_my_reservations.sql
@@ -8,6 +8,7 @@ SELECT reservations.id, title,cost_per_night, start_date,AVG(rating) AS average_
 JOIN reservations ON properties.id=reservations.property_id
 JOIN property_reviews ON properties.id=property_reviews.property_id
 WHERE reservations.guest_id=1
-GROUP BY reservations.id,title,cost_per_night
-ORDER BY start_date DESC
+GROUP BY reservations.id,properties.id
+ORDER BY start_date
 LIMIT 10;
+
