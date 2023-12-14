2ade56cc44fbe456eb595ee21437097824234e0a
dhaaranisuriya2605@gmail.com <dhaaranisuriya2605@gmail.com>
2023-12-13 17:10:00 -0800

    add comments

--- 1_queries/4_most_visited_cities.sql
+++ 1_queries/4_most_visited_cities.sql
@@ -1,3 +1,7 @@
+-- Get a list of the most visited cities.
+-- Select the name of the city and the number of reservations for that city.
+-- Order the results from highest number of reservations to lowest number of reservations.
+
 SELECT city,COUNT(reservations.id) AS total_reservations FROM properties
 JOIN reservations ON reservations.property_id=properties.id
 GROUP BY city
